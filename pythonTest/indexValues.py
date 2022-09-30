26f58f91bd96a0499c7a6e2eb60a4b8192679857
Nan <nan1ayom@gmail.com>
2022-09-30 08:30:02 +0800

    temporary save

--- pythonTest/indexValues.py
+++ pythonTest/indexValues.py
@@ -1,4 +1,11 @@
 import torch
+import random
+
+def h():
+    print('feng')
+    # return 9
+    yield 12
+    print('yes')
 if __name__ == '__main__':
     test_batch = [[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6]]
-    X  = torch.rand(2,20)
+    h()
